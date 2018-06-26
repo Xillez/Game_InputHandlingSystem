10aab30befd03f78abe4dbdd03496f5d030e1118
Xillez <kentwh@stud.ntnu.no>
2018-06-27 01:21:24 +0200

    Added, implemented and documented all funtions in 'InputHandler' .hpp and .cpp

--- source/input/InputHandler.cpp
+++ source/input/InputHandler.cpp
@@ -1,1 +1,90 @@
 #include "InputHandler.hpp"
+
+InputHandler::InputHandler()
+{
+    // Reserve for quicker registration.
+    this->keys.reserve(25);
+
+    // Save pointer to window.
+    this->window = nullptr;
+}
+
+InputHandler::InputHandler(GLFWwindow* window)
+{
+    // Reserve for quicker registration.
+    this->keys.reserve(25);
+
+    // Save pointer to window.
+    this->window = window;
+}
+
+void InputHandler::setCurrentWindow(GLFWwindow* window)
+{
+    // Save pointer to window.
+    this->window = window;
+}
+
+bool InputHandler::registerKey(Key key)
+{
+    // If the key doesn't exists.
+    if (!isRegistered(key))
+    {
+        // Register it and set it as active. 
+        this->keys.push_back(key);
+        this->keystate[key] = std::pair<bool, bool>(true, false);
+        return true;
+    }
+    // It already exists. 
+    return false;
+}
+
+bool InputHandler::unregisterKey(Key key)
+{
+    // If the key doesn't exists.
+    if (isRegistered(key))
+    {
+        // Delete it.
+        this->keys.erase(std::find(this->keys.begin(), this->keys.end(), key));
+        this->keystate.erase(key);
+        return true;
+    }
+    // Couldn't find it or failed. 
+    return false;
+}
+
+void InputHandler::update()
+{
+    //for (auto& item : this->keystate)
+    //    ;
+}
+
+bool InputHandler::getKeyState(Key key)
+{
+    if (isRegistered(key))
+        return this->keystate[*std::find(this->keys.begin(), this->keys.end(), key)].second;
+    return false;
+}
+
+bool InputHandler::isRegistered(Key key)
+{
+    return (std::find(this->keys.begin(), this->keys.end(), key) != this->keys.end());
+}
+
+bool InputHandler::isActive(Key key)
+{
+    if (this->keystate.find(key) != this->keystate.end())
+        return this->keystate.find(key)->second.first;
+    return false;
+}
+
+void InputHandler::activate(Key key)
+{
+    if (isRegistered(key))
+        this->keystate.find(key)->second.second = true;
+}
+
+void InputHandler::deactivate(Key key)
+{
+    if (isRegistered(key))
+        this->keystate.find(key)->second.second = false;
+}
--- source/input/InputHandler.hpp
+++ source/input/InputHandler.hpp
@@ -2,13 +2,12 @@
 
 #include "InputInterface.hpp"
 
-#include <vector>
-#include <unordered_map>
-#include <algorithm>
-#include <cstdlib>
-#include <utility>
-#include <tuple>
-#include <string>
+#include <string>			// string
+#include <vector>			// vector
+#include <unordered_map>	// unordered_map
+#include <utility>			// pair
+#include <algorithm>		// find
+#include <GLFW/glfw3.h>
 
 /**
  * @brief General InputHandler class. 
@@ -21,13 +20,40 @@ public:
 	 * @brief InputHandler constructor.
 	 */
 	InputHandler();
+	
+	/**
+	 * @brief InputHandler constructor.
+	 * 
+	 * @param window - GLFWwindow* - window to listen to.
+	 */
+	InputHandler(GLFWwindow* window);
+
+	/**
+	 * @brief Set the current window listen to.
+	 * 
+	 * @param window - GLFWwindow* - Window to listen to.
+	 */
+	void setCurrentWindow(GLFWwindow* window);
 
 	/**
 	 * @brief A function for registering keys to check.
 	 * 
-	 * @param key - Key (enum) - key to check for.
+	 * @param key - Key (enum) - Key to check for.
+	 * 
+	 * @return true - If key registered.
+	 * @return false - If failed to register.
 	 */
 	bool registerKey(Key key);
+
+	/**
+	 * @brief Unregisters the specified key
+	 * 
+	 * @param key - Key (enum) - Key to unregister.
+	 * 
+	 * @return true - If unregistering wa successfull
+	 * @return false - If it was unsuccessfull.
+	 */
+	bool unregisterKey(Key key);
 
 	/**
 	 * @brief Updates state for next frame.
@@ -37,18 +63,70 @@ public:
 	/**
 	 * @brief A function for getting key state.
 	 * 
-	 * @param key - Key (enum) - wanted key.
+	 * @param key - Key (enum) - Wanted key.
+	 * 
+	 * @return true - Key down.
+	 * @return false - Key up.
 	 */
 	bool getKeyState(Key key);
 
 	/**
-	 * @brief Function to convert key to printable string.
+	 * @brief A function for getting key.
 	 * 
-	 * @param key - Key (enum) - key to convert.
+	 * @param key - Key (enum) - Wanted key.
+	 * 
+	 * @return A copy of the wanted key if found, error key if not.
+	 */
+	//Key getKey(std::string key);
+
+	/**
+	 * @brief A function to check if key is registered.
+	 * 
+	 * @param key - Key (enum) - Key to check.
+	 * 
+	 * @return true - If registered.
+	 * @return false - Not registered.
+	 */
+	bool isRegistered(Key key);
+
+	/**
+	 * @brief A function to check if key is active.
+	 * 
+	 * @param key - Key (enum) - Key to check.
+	 * 
+	 * @return true - If active.
+	 * @return false - Not active.
+	 */
+	bool isActive(Key key);
+
+	/**
+	 * @brief A function to activate a key.
+	 * 
+	 * @param key - Key (enum) - Key to activate.
+	 */
+	void activate(Key key);
+
+	/**
+	 * @brief A function to deactivate a key.
+	 * 
+	 * @param key - Key (enum) - Key to deactivate.
+	 */
+	void deactivate(Key key);
+
+	/**
+	 * @brief Function to convert Key to printable string.
+	 * 
+	 * @param key - Key (enum) - Key to convert.
+	 * 
 	 * @return std::string - Keys string equivalent.
 	 */
 	//std::string keyToString(Key key);
 
-private:
-	std::unordered_map<Key, bool> keystate;							//!< Posistion of entity.
+	static void onKeyPressed(GLFWwindow* window, int key, int scancode, int action, int mods);
+
+protected:
+	std::vector<Key> keys;										//!< Keys registered.
+	std::unordered_map<Key, std::pair<bool, bool>> keystate;	//!< The state of the registered key.
+
+	GLFWwindow* window;
 };
